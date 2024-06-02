5fb006f7f20b18a8d3ee85bda61580f804701b83
BiggerH2 <161705584+BiggerH2@users.noreply.github.com>
2024-06-02 15:12:28 +0300

    updated

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    """Write content to a file."""
+    with open(f"{file_name}.txt", mode='w', encoding='utf-8') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """Append content to a file."""
+    with open(f"{file_name}.txt", mode='a', encoding='utf-8') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    """Read content from a file."""
+    with open(f"{file_name}.txt", encoding='utf-8') as file:
+        return file.read()
